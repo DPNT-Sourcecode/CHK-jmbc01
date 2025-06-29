d7d474e7b41b7e5921f957473cbe6c2c5a45bcf6
sbx_user1051 <sbx_user1051@169.254.13.239>
2025-06-29 12:17:04 +0000

    Sun Jun 29 12:17:04 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -54,16 +54,28 @@ class CheckoutSolution:
 
     def has_special_offer(self, item: dict) -> bool:
         is_in_special_offers = item['sku'] in SPECIAL_OFFERS
-        amount_qualifies = item['count'] >= SPECIAL_OFFERS[item['sku']]['amount']
+        amount_qualifying = SPECIAL_OFFERS[item['sku']]['amount']
+        amount_qualifies = item['count'] >= amount_qualifying
         return is_in_special_offers and amount_qualifies
+
+    def calculate_with_special_offer(self, item: dict) -> int:
+        special_offer = SPECIAL_OFFERS[item['sku']]
+        # how many sets of products could qualify for the special offer
+        offer_count = item['count'] // special_offer['amount']
+        total_with_offer = offer_count * special_offer['price']
+        # remaining products
+        remaining_count = item['count'] % special_offer['amount']
+        remaining_total = item['price'] * remaining_count
+        return total_with_offer + remaining_total
 
     def calculate_total(self, sub_totals: list) -> int:
         total = 0
         for item in sub_totals:
             if self.has_special_offer(item):
-                total -= self.apply_special_offer(item)
+                total += self.calculate_with_special_offer(item)
             total += item['price'] * item['count']
 
         return total
+
 
 
