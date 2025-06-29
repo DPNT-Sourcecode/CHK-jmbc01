966fc9dcde8af8ad85198cc01cac3ca38ec75e49
sbx_user1051 <sbx_user1051@169.254.13.239>
2025-06-29 10:36:51 +0000

    Sun Jun 29 10:36:51 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,10 @@
 
 class SumSolution:
-    
+
     def compute(self, x, y):
-        raise NotImplementedError()
+        if x is None or y is None:
+            raise ValueError("Both x and y must be provided")
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both x and y must be numbers")
+        return x + y
+
