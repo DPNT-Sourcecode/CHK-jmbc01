455021109a2a731bbd71ffed9969b05001fc7b91
sbx_user1051 <sbx_user1051@169.254.13.239>
2025-06-29 11:16:14 +0000

    Sun Jun 29 11:16:14 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -7,11 +7,6 @@ class HelloSolution:
             raise ValueError("friend_name cannot be empty")
         if not isinstance(friend_name, str):
             raise TypeError("friend_name parameter must be a string")
-        try:
-            friend_name.encode('utf-8')
-            print("friend_name is a valid unicode string")
-        except UnicodeEncodeError:
-            raise UnicodeEncodeError("friend_name must be a unicode string")
-
         return f"Hello, {friend_name}!"
+
 
--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -9,10 +9,6 @@ class TestHello():
     def test_hello_with_name(self):
         assert HelloSolution().hello("Bob") == "Hello, Bob!"
 
-    def test_hello_with_special_characters(self):
-        with pytest.raises(UnicodeDecodeError):
-            HelloSolution().hello(u"Example Characters : \xc3\xa9 \xc3\xa0 Ã© Ã")
-
     def test_empty_string(self):
         with pytest.raises(ValueError):
             HelloSolution().hello("")
@@ -20,5 +16,6 @@ class TestHello():
     def test_string(self):
         with pytest.raises(TypeError):
             HelloSolution().hello(123)
+
 
 
