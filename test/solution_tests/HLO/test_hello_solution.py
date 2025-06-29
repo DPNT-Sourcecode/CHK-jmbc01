4c9056bdf3a5fd4a45f9736418396e888ec4b6ba
sbx_user1051 <sbx_user1051@169.254.13.239>
2025-06-29 11:01:14 +0000

    Sun Jun 29 11:01:14 UTC 2025

--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -4,7 +4,14 @@ import pytest
 
 class TestHello():
     def test_hello(self):
-        assert HelloSolution().hello("Alice") == "Hello, World!"
+        assert 'Hello,' in HelloSolution().hello("Alice")
+
+    def test_hello_with_name(self):
+        assert HelloSolution().hello("Bob") == "Hello, Bob!"
+
+    def test_hello_with_special_characters(self):
+        with pytest.raises(UnicodeDecodeError):
+            HelloSolution().hello("Ã© Ã")
 
     def test_empty_string(self):
         with pytest.raises(ValueError):
@@ -13,5 +20,6 @@ class TestHello():
     def test_string(self):
         with pytest.raises(TypeError):
             HelloSolution().hello(123)
+
 
 
