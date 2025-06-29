45420a18c8a36f1a51f2b763b5b64420664d0dc3
sbx_user1051 <sbx_user1051@169.254.67.31>
2025-06-29 19:07:31 +0000

    Sun Jun 29 19:07:31 UTC 2025

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -68,10 +68,12 @@ class TestCheckout():
 
     def test_free_item_same_as_trigger(self):
         assert CheckoutSolution().checkout("F") == 10
-        assert CheckoutSolution().checkout("FF") == 10
+        assert CheckoutSolution().checkout("FF") == 20
         assert CheckoutSolution().checkout("FFF") == 20
-        assert CheckoutSolution().checkout("FFFF") == 10 + 10
-        assert CheckoutSolution().checkout("FFFFF") == 10 + 20
+        assert CheckoutSolution().checkout("FFFF") == 20 + 10
+        assert CheckoutSolution().checkout("FFFFF") == 20 + 20
+        assert CheckoutSolution().checkout("FFFFFF") == 20 + 20
+
 
 
 
