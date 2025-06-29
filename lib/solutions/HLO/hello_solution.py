8b5c37b4d8abd4090f127241216449511338af23
sbx_user1051 <sbx_user1051@169.254.13.239>
2025-06-29 10:49:28 +0000

    Sun Jun 29 10:49:28 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,4 +3,9 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name):
-        raise NotImplementedError()
+        if not friend_name:
+            raise ValueError("friend_name cannot be empty")
+        if not isinstance(friend_name, str):
+            raise TypeError("friend_name parameter must be a string")
+        return f"'Hello, world!' from {friend_name}!"
+
---
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -0,0 +1,15 @@
+from solutions.HLO.hello_solution import HelloSolution
+import pytest
+
+
+class TestHello():
+    def test_hello(self):
+        assert HelloSolution().hello("Alice") == "'Hello, world!' from Alice!"
+
+    def test_empty_string(self):
+        with pytest.raises(ValueError):
+            HelloSolution().hello("")
+
+    def test_string(self):
+        with pytest.raises(TypeError):
+            HelloSolution().hello(123)
